3c1448c6961b2ad4b9158052626cfb3d04aec077
Az3rAp1 <jjj.vankaam@student.avans.nl>
2025-03-24 19:19:23 +0100

    Smarthealth-Api | models | added models

---
+++ smarth-health.WebApi/Models/TimeLineItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace smarth_health.WebApi.Models
+{
+    public class TimeLineItem
+    {
+        public Guid ID;
+        public String Content;
+        public bool Video;
+        public String VideoPath;
+        public bool Medicine;
+        public String ToolTipContent;
+        public String ImagePath;
+        public String Position;
+        public Guid TimeLineID;
+
+    }
+}
---
+++ smarth-health.WebApi/Models/Timeline.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+namespace smarth_health.WebApi.Models
+{
+    public class Timeline
+    {
+        public Guid ID;
+        public String Name;
+        public bool RouteType;
+        public Guid UserID;
+    }
+}
