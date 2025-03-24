2cf1b5ba796d21821090f49988bd43a5fa5235d4
Az3rAp1 <jjj.vankaam@student.avans.nl>
2025-03-24 20:29:56 +0100

    add sanetizing and handling

--- smarth-health.WebApi/Repositories/TimeLineRepository.cs
+++ smarth-health.WebApi/Repositories/TimeLineRepository.cs
@@ -1,5 +1,4 @@
-﻿// TimeLineItemRepository.cs
-using Dapper;
+﻿using Dapper;
 using smarth_health.WebApi.Models;
 using System.Data;
 
@@ -28,6 +27,8 @@ namespace smarth_health.WebApi.Repositories
 
         public async Task CreateAsync(TimeLineItem item)
         {
+            SanitizeItemStrings(item);
+
             var sql = @"
             INSERT INTO TimeLineItems (ID, Content, Video, VideoPath, Medicine, ToolTipContent, ImagePath, Position, TimeLineID)
             VALUES (@ID, @Content, @Video, @VideoPath, @Medicine, @ToolTipContent, @ImagePath, @Position, @TimeLineID)";
@@ -37,6 +38,8 @@ namespace smarth_health.WebApi.Repositories
 
         public async Task UpdateAsync(TimeLineItem item)
         {
+            SanitizeItemStrings(item);
+
             var sql = @"
             UPDATE TimeLineItems
             SET Content = @Content,
@@ -56,6 +59,16 @@ namespace smarth_health.WebApi.Repositories
         {
             var sql = "DELETE FROM TimeLineItems WHERE ID = @Id";
             await _dbConnection.ExecuteAsync(sql, new { Id = id });
+        }
+
+        private void SanitizeItemStrings(TimeLineItem item)
+        {
+            item.Content = item.Content?.Trim();
+            item.Video = item.Video?.Trim();
+            item.VideoPath = item.VideoPath?.Trim();
+            item.Medicine = item.Medicine?.Trim();
+            item.ToolTipContent = item.ToolTipContent?.Trim();
+            item.ImagePath = item.ImagePath?.Trim();
         }
     }
 }
