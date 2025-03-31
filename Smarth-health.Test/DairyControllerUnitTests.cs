894d502afc773eef9ba4adcd877014d6c32f9b7d
Az3rAp1 <jjj.vankaam@student.avans.nl>
2025-03-31 11:38:38 +0200

    API | Tests | added right unit test project and added unit tests for all controllers

---
+++ Smarth-health.Test/DairyControllerUnitTests.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using smarth_health.WebApi.Controllers;
+using smarth_health.WebApi.Models;
+using smarth_health.WebApi.Repositories;
+
+namespace smarth_health.Tests
+{
+    [TestClass]
+    public sealed class DairyControllerTests
+    {
+        private Mock<IDairyRepository> _dairyRepository;
+        private DairyController _controller;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _dairyRepository = new Mock<IDairyRepository>();
+            _controller = new DairyController(_dairyRepository.Object);
+        }
+
+        [TestMethod]
+        public async Task GetAllDairies_ReturnsOkObjectResult_WithListOfDairies()
+        {
+            // ARRANGE
+            var dairies = new List<Dairy>
+            {
+                new Dairy { Id = Guid.NewGuid() },
+                new Dairy { Id = Guid.NewGuid() }
+            };
+            _dairyRepository.Setup(repo => repo.GetAllAsync()).ReturnsAsync(dairies);
+
+            // ACT
+            var result = await _controller.GetAllDairies();
+
+            // ASSERT
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult, "Expected OkObjectResult");
+            Assert.AreEqual(StatusCodes.Status200OK, okResult.StatusCode);
+            Assert.AreEqual(dairies, okResult.Value);
+        }
+
+        [TestMethod]
+        public async Task GetDairy_ExistingDairy_ReturnsOkObjectResult()
+        {
+            // ARRANGE
+            var id = Guid.NewGuid();
+            var dairy = new Dairy { Id = id };
+            _dairyRepository.Setup(repo => repo.GetByIdAsync(id)).ReturnsAsync(dairy);
+
+            // ACT
+            var result = await _controller.GetDairy(id);
+
+            // ASSERT
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult, "Expected OkObjectResult");
+            Assert.AreEqual(StatusCodes.Status200OK, okResult.StatusCode);
+            Assert.AreEqual(dairy, okResult.Value);
+        }
+
+        [TestMethod]
+        public async Task GetDairy_NonExistingDairy_ReturnsNotFoundResult()
+        {
+            // ARRANGE
+            var id = Guid.NewGuid();
+            _dairyRepository.Setup(repo => repo.GetByIdAsync(id)).ReturnsAsync((Dairy)null);
+
+            // ACT
+            var result = await _controller.GetDairy(id);
+
+            // ASSERT
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+        }
+
+        [TestMethod]
+        public async Task CreateDairy_ValidDairy_ReturnsCreatedAtActionResult()
+        {
+            // ARRANGE
+            var dairy = new Dairy(); // Initially no Id assigned.
+            _dairyRepository.Setup(repo => repo.CreateAsync(It.IsAny<Dairy>()))
+                           .Returns(Task.CompletedTask);
+
+            // ACT
+            var result = await _controller.CreateDairy(dairy);
+
+            // ASSERT
+            var createdResult = result as CreatedAtActionResult;
+            Assert.IsNotNull(createdResult, "Expected CreatedAtActionResult");
+            Assert.AreEqual(nameof(_controller.GetDairy), createdResult.ActionName);
+
+            var returnedDairy = createdResult.Value as Dairy;
+            Assert.IsNotNull(returnedDairy, "Returned dairy should not be null.");
+            Assert.AreNotEqual(Guid.Empty, returnedDairy.Id, "Expected dairy Id to be assigned a new Guid.");
+
+            _dairyRepository.Verify(repo => repo.CreateAsync(It.IsAny<Dairy>()), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task CreateDairy_InvalidModelState_ReturnsBadRequest()
+        {
+            // ARRANGE
+            var dairy = new Dairy();
+            _controller.ModelState.AddModelError("error", "Invalid dairy");
+
+            // ACT
+            var result = await _controller.CreateDairy(dairy);
+
+            // ASSERT
+            Assert.IsInstanceOfType(result, typeof(BadRequestObjectResult));
+        }
+
+        [TestMethod]
+        public async Task UpdateDairy_IdMismatch_ReturnsBadRequest()
+        {
+            // ARRANGE
+            var id = Guid.NewGuid();
+            var dairy = new Dairy { Id = Guid.NewGuid() };
+
+            // ACT
+            var result = await _controller.UpdateDairy(id, dairy);
+
+            // ASSERT
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+        }
+
+        [TestMethod]
+        public async Task UpdateDairy_NonExistingDairy_ReturnsNotFoundResult()
+        {
+            // ARRANGE
+            var id = Guid.NewGuid();
+            var dairy = new Dairy { Id = id };
+
+            _dairyRepository.Setup(repo => repo.GetByIdAsync(id))
+                           .ReturnsAsync((Dairy)null);
+
+            // ACT
+            var result = await _controller.UpdateDairy(id, dairy);
+
+            // ASSERT
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+        }
+
+        [TestMethod]
+        public async Task UpdateDairy_ValidDairy_ReturnsNoContentResult()
+        {
+            // ARRANGE
+            var id = Guid.NewGuid();
+            var dairy = new Dairy { Id = id };
+
+            _dairyRepository.Setup(repo => repo.GetByIdAsync(id))
+                           .ReturnsAsync(dairy);
+            _dairyRepository.Setup(repo => repo.UpdateAsync(dairy))
+                           .Returns(Task.CompletedTask);
+
+            // ACT
+            var result = await _controller.UpdateDairy(id, dairy);
+
+            // ASSERT
+            Assert.IsInstanceOfType(result, typeof(NoContentResult));
+            _dairyRepository.Verify(repo => repo.UpdateAsync(dairy), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task DeleteDairy_NonExistingDairy_ReturnsNotFoundResult()
+        {
+            // ARRANGE
+            var id = Guid.NewGuid();
+            _dairyRepository.Setup(repo => repo.GetByIdAsync(id))
+                           .ReturnsAsync((Dairy)null);
+
+            // ACT
+            var result = await _controller.DeleteDairy(id);
+
+            // ASSERT
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+        }
+
+        [TestMethod]
+        public async Task DeleteDairy_ExistingDairy_ReturnsNoContentResult()
+        {
+            // ARRANGE
+            var id = Guid.NewGuid();
+            var dairy = new Dairy { Id = id };
+
+            _dairyRepository.Setup(repo => repo.GetByIdAsync(id))
+                           .ReturnsAsync(dairy);
+            _dairyRepository.Setup(repo => repo.DeleteAsync(id))
+                           .Returns(Task.CompletedTask);
+
+            // ACT
+            var result = await _controller.DeleteDairy(id);
+
+            // ASSERT
+            Assert.IsInstanceOfType(result, typeof(NoContentResult));
+            _dairyRepository.Verify(repo => repo.DeleteAsync(id), Times.Once);
+        }
+    }
+}
---
+++ Smarth-health.Test/UserControllerUnitTests.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using smarth_health.WebApi.Controllers;
+using smarth_health.WebApi.Models;
+using smarth_health.WebApi.Repositories;
+
+namespace smarth_health.Tests
+{
+    [TestClass]
+    public sealed class UserControllerTests
+    {
+        private Mock<IUserRepository> _userRepository;
+        private UserController _controller;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _userRepository = new Mock<IUserRepository>();
+            _controller = new UserController(_userRepository.Object);
+        }
+
+        [TestMethod]
+        public async Task CreateUser_ValidUser_ReturnsCreatedAtActionResult()
+        {
+            // ARRANGE
+            var user = new User { IdentityUserID = Guid.NewGuid() };
+            _userRepository.Setup(repo => repo.InsertAsync(It.IsAny<User>()))
+                           .Returns(Task.CompletedTask);
+
+            // ACT
+            var result = await _controller.CreateUser(user);
+
+            // ASSERT
+            var createdResult = result as CreatedAtActionResult;
+            Assert.IsNotNull(createdResult, "Expected CreatedAtActionResult");
+            Assert.AreEqual("GetUser", createdResult.ActionName);
+            Assert.IsNotNull(createdResult.RouteValues);
+            Assert.IsTrue(createdResult.RouteValues.ContainsKey("identityUserId"));
+            Assert.AreEqual(user.IdentityUserID, createdResult.RouteValues["identityUserId"]);
+
+            var returnedUser = createdResult.Value as User;
+            Assert.IsNotNull(returnedUser);
+            Assert.AreNotEqual(Guid.Empty, returnedUser.ID, "User ID should be assigned a new Guid.");
+
+            _userRepository.Verify(repo => repo.InsertAsync(It.IsAny<User>()), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task GetUser_UserExists_ReturnsOkObjectResult()
+        {
+            // ARRANGE
+            var identityUserId = Guid.NewGuid();
+            var user = new User { ID = Guid.NewGuid(), IdentityUserID = identityUserId };
+            _userRepository.Setup(repo => repo.ReadAsync(identityUserId))
+                           .ReturnsAsync(user);
+
+            // ACT
+            var result = await _controller.GetUser(identityUserId);
+
+            // ASSERT
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult, "Expected OkObjectResult");
+            Assert.AreEqual(user, okResult.Value);
+        }
+
+        [TestMethod]
+        public async Task GetUser_UserDoesNotExist_ReturnsNotFoundResult()
+        {
+            // ARRANGE
+            var identityUserId = Guid.NewGuid();
+            _userRepository.Setup(repo => repo.ReadAsync(identityUserId))
+                           .ReturnsAsync((User)null);
+
+            // ACT
+            var result = await _controller.GetUser(identityUserId);
+
+            // ASSERT
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+        }
+
+        [TestMethod]
+        public async Task UpdateUser_IdMismatch_ReturnsBadRequest()
+        {
+            // ARRANGE
+            var userId = Guid.NewGuid();
+            var user = new User { ID = Guid.NewGuid() };
+
+            // ACT
+            var result = await _controller.UpdateUser(userId, user);
+
+            // ASSERT
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+        }
+
+        [TestMethod]
+        public async Task UpdateUser_ValidUser_ReturnsNoContentResult()
+        {
+            // ARRANGE
+            var userId = Guid.NewGuid();
+            var user = new User { ID = userId };
+
+            _userRepository.Setup(repo => repo.UpdateAsync(user))
+                           .Returns(Task.CompletedTask);
+
+            // ACT
+            var result = await _controller.UpdateUser(userId, user);
+
+            // ASSERT
+            Assert.IsInstanceOfType(result, typeof(NoContentResult));
+            _userRepository.Verify(repo => repo.UpdateAsync(user), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task DeleteUser_ReturnsNoContentResult()
+        {
+            // ARRANGE
+            var userId = Guid.NewGuid();
+            _userRepository.Setup(repo => repo.DeleteAsync(userId))
+                           .Returns(Task.CompletedTask);
+
+            // ACT
+            var result = await _controller.DeleteUser(userId);
+
+            // ASSERT
+            Assert.IsInstanceOfType(result, typeof(NoContentResult));
+            _userRepository.Verify(repo => repo.DeleteAsync(userId), Times.Once);
+        }
+    }
+}
